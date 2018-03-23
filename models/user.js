34fe78fce121945c9f6f6e241664701552d27347
Stephanie Basaraba <stephanie.basaraba@gmail.com>
2018-03-22 20:30:14 -0500

    working server based off of cats to be updated

--- models/user.js
+++ models/user.js
@@ -2,10 +2,6 @@ var mongoose = require ("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose")
 
 
-
-
-
-
 var UserSchema = new mongoose.Schema({
     username: String,
     password: String
@@ -15,3 +11,33 @@ var UserSchema = new mongoose.Schema({
 UserSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User",UserSchema)
+
+
+var orm = require("../config/orm.js");
+
+var cat = {
+  all: function(cb) {
+    orm.all("cats", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  create: function(cols, vals, cb) {
+    orm.create("cats", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVals, condition, cb) {
+    orm.update("cats", objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+  delete: function(condition, cb) {
+    orm.delete("cats", condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+// Export the database functions for the controller (catsController.js).
+module.exports = cat;
