1ce96268c96f1396c250a952fbf64005c9d78d76
jinana3 <jinali.berkeley@gmail.com>
2018-03-24 14:56:37 -0500

    saturday's work

--- Controllers/reserve_controller.js
+++ Controllers/reserve_controller.js
@@ -1,25 +1,32 @@
-//in sequelize, the class activity exercises used "routes"
-//so reference week15 exercises' routes folder for what to input here
-//also know that handlebars will be referencing this file
-//refer to week 14 to see how that's done
-
-
 //this file offers a set of routes for displaying and saving data to db
 var db = require("../models");
 
 
 module.exports = function(app){
 
+
+  //front page reserve.html doesn't need anything
+
+  //creating new row in sql for new registration
+  app.post("/api/registration", function(req,res){
+    //if grocery then grocery table else store table
+    //register.html
+    if (req.body.type === 'foodbank'){ //need to add type into sql
+      db.foodbank.create(req.body).then(function(db) {
+        res.json(db);
+      });
+    }
+
+  });
+
+
+  //verifying login information against sql
   app.get("/api/login", function(req,res){
     //if grocery then grocery table else store table
     //using login information from tables 
     //login.html
-  })
+  });
 
-  app.post("/api/register", function(req,res){
-    //if grocery then grocery table else store table
-    //using login information from tables 
-    //register.html
-  })
+
+
 }
-
