c18598c39d6ee5c4e2d5178b06ab0f85fc2359e2
jinana3 <jinali.berkeley@gmail.com>
2018-03-24 09:09:53 -0500

    sequelize init

---
+++ data/schema.sql
@@ -0,0 +1,4 @@
+-- Drops the todolist if it exists currently --
+DROP DATABASE IF EXISTS reserve;
+-- Creates the "todolist" database --
+CREATE DATABASE reserve;
---
+++ models/reserved.js
@@ -0,0 +1,20 @@
+//this script for creating sequeqlize model
+//for the reserved table in reserve database
+
+//reference structure:
+module.exports = function(sequelize, DataTypes) {
+  var  = sequelize.define("Author", {
+    // Giving the Author model a name of type STRING
+    name: DataTypes.STRING
+  });
+
+  Author.associate = function(models) {
+    // Associating Author with Posts
+    // When an Author is deleted, also delete any associated Posts
+    Author.hasMany(models.Post, {
+      onDelete: "cascade"
+    });
+  };
+
+  return Author;
+};
