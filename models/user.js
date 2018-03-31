dc7158a5836b0b0f85463b7241f8f572e7f15b45
stephaniebasaraba <31085165+stephaniebasaraba@users.noreply.github.com>
2018-03-31 11:36:31 -0500

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,31 +1,39 @@
-//setting up the sequelize for the user table
 module.exports = function(sequelize, DataTypes) {
     var User = sequelize.define("user", {
-        id: {
+        //reference:
+        //https://code.tutsplus.com/tutorials/using-passport-with-sequelize-and-mysql--cms-27537
+
+        id: { //not sure if this is needed
             autoIncrement: true,
             primaryKey: true,
             type: DataTypes.INTEGER
         },
+
         usertype: {
             type: DataTypes.ENUM('grocery', 'pantry'),
             defaultValue: 'Grocery'
         },
+
         name: {
             type: DataTypes.STRING,
             notEmpty: true
         },
+
         addressNumber: {
             type: DataTypes.STRING,
             notEmpty: false
         },
+
         addressStreet: {
             type: DataTypes.STRING,
             notEmpty: true
         },
+
         city: {
             type: DataTypes.STRING,
             notEmpty: true
         },
+
         state: {
             type: DataTypes.STRING,
             notEmpty: true
@@ -44,20 +52,24 @@ module.exports = function(sequelize, DataTypes) {
             type: DataTypes.STRING,
             allowNull: false
         },
+
         last_login: {
             type: DataTypes.DATE
         },
-        status: { 
+
+        status: { // not sure if this is needed
             type: DataTypes.ENUM('active', 'inactive'),
             defaultValue: 'active'
         }
-    });
-    //associating user with inventory table 
-    User.associate = function(models) {
 
+    });
+    User.associate = function(models) {
+       // Associating Author with Posts
+       // When an Author is deleted, also delete any associated Posts
         User.hasMany(models.inventory, {
             onDelete: "cascade"
         });
     };
+
     return User;
 };
